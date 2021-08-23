8744078d1885ea06ddd5211728658bf5fe689c00
y-hayamizu <yusaku.hayamizu@ieee.org>
2021-08-23 13:31:08 +0900

    scenario C-2

---
+++ practice-C/start-C-2.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# mkdir -p /tmp/log
+
+export node_bindir=$(mktemp -d /tmp/tmp.XXXXXXXXX)
+# trap '[[ -d "${node_bindir:?}" && "${node_bindir::4}" = "/tmp" ]] && rm -rf "${node_bindir:?}"' EXIT
+cp -r bin $node_bindir
+
+docker-compose build
+docker-compose up -d
+docker ps -a | sed -E 's/ ( +)/\1|/g' | cut -f 1,2,4,5 -d "|" | sed -E 's/ +$//g'
+
+docker exec push-consumer /cefore/bin/push-consumer.bash
+
