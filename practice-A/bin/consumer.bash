1a9610f109c4cc89a1c78cacc176f7112f470a59
y-hayamizu <yusaku.hayamizu@ieee.org>
2021-08-26 19:02:14 +0900

    modified yml

--- practice-A/bin/consumer.bash
+++ practice-A/bin/consumer.bash
@@ -6,7 +6,7 @@ ifstat -t > /tmp/log/throughput.log &
 
 # [A-1] Aglobal scenario
 # cefroute add ccnx:/stream udp 133.69.33.120
-cefroute add ccnx:/stream tcp 133.69.33.120
+cefroute add ccnx:/stream udp 133.69.33.120
 
 # [A-2] Local scenario
 # cefroute add ccnx:/stream tcp 10.0.1.10
--- practice-C/start-C-1.bash
+++ practice-C/start-C-1.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# mkdir -p /tmp/log
+mkdir -p /tmp/log
 
 export node_bindir=$(mktemp -d /tmp/tmp.XXXXXXXXX)
 # trap '[[ -d "${node_bindir:?}" && "${node_bindir::4}" = "/tmp" ]] && rm -rf "${node_bindir:?}"' EXIT
